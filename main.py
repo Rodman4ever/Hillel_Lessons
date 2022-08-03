001aa35a4d2ca8fcb1d4934e08e29ef361a1bed5
origin <git@github.com:Rodman4ever/Hillel_Lessons.git>
2022-08-04 00:13:07 +0300

    1 home work

---
+++ main.py
@@ -0,0 +1,26 @@
+#from __future__ import braces point 3.d of home work this is a joke - an easter egg There will never be brackets in the language
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import second
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+import this
+# import this - point 3.a of home work Zen of Python presents a set of 19 guidelines for writing computer programs
+
+import antigravity
+#import antigravity - point 3.b of home work The command opens an XKCD comic that mentions Python in the browser.
+
+import __hello__
+#import __hello__ point 3.c of home work print the most famous phrase in programming
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
