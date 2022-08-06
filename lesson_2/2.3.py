123f19ff1537453b2d2213a75df2c0833a790be5
origin <git@github.com:Rodman4ever/Hillel_Lessons.git>
2022-08-06 20:55:54 +0300

    2.3 exercise wrong code

---
+++ lesson_2/2.3.py
@@ -0,0 +1,3 @@
+v =int(input('Please enter the speed of the cyclist:'))
+t =int(input('Please enter the time of the cyclist:'))
+print('In ' + str(v) + ' hours the cyclist will be at the mark ' + str(v*t) + ' km')
