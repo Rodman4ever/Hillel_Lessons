e8823bdb0040768e29d9e97742c0a630e9d80319
origin <git@github.com:Rodman4ever/Hillel_Lessons.git>
2022-08-06 20:20:15 +0300

    2.2 exercise complete

---
+++ lesson_2/2_2.py
@@ -0,0 +1,3 @@
+asked_from_user =int(input('Please enter an integer number:'))
+print('The next number for the number ' + str(asked_from_user) + ' is ' + str(asked_from_user + 1) + '.')
+print('The previous number for the number ' + str(asked_from_user) + ' is ' + str(asked_from_user - 1) + '.')
