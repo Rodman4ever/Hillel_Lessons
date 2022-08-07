18ed988699b09dc0a56a6a96ec4a16b179f9d040
origin <git@github.com:Rodman4ever/Hillel_Lessons.git>
2022-08-07 17:34:24 +0300

    error in 2_3 correction

--- lesson_2/2_3.py
+++ lesson_2/2_3.py
@@ -1,8 +1,9 @@
 v = int(input('Please enter the speed of the cyclist:'))
 t = int(input('Please enter the time of the cyclist:'))
+m = 100  # довжина маршруту
 x = v * t
 if v <= 0:
-    print(0)
-# відємне значення швидкості згідно з завданням допустиме, проте вірною відповіддю мав в даному випадку бути 0
+    print(m - x)
+
 else:
-    print(v * t)  # отримуємо число а не стрічку
+    print(x)
