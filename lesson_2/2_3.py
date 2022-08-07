ebc0b2322cdfc2d752770f0904ce1eea196a423f
origin <git@github.com:Rodman4ever/Hillel_Lessons.git>
2022-08-07 21:43:05 +0300

    error in 2_3 last one correction

--- lesson_2/2_3.py
+++ lesson_2/2_3.py
@@ -3,7 +3,7 @@ t = int(input('Please enter the time of the cyclist:'))
 m = 100  # довжина маршруту
 x = v * t
 if v <= 0:
-    print(m - - x) # від"ємне значення!
+    print(m - - x)  # від"ємне значення!
 
 else:
     print(x)
